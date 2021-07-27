7800be8b8afffb1441c8a451e0603782d76dce0a
FONYUY <61058194+fonyuy314@users.noreply.github.com>
2021-07-27 16:28:59 +0100

    added content to html and in index.js file

---
+++ project2/js/index.js
@@ -0,0 +1,24 @@
+// navigation
+const navOpen = document.querySelector(".nav__humburger")
+const navClose = document.querySelector(".close__toggle")
+const menu = document.querySelector(".nav__menu")
+const navContainer = document.querySelector(".nav__menu")
+
+navOpen.addEventListener("click", () => {
+    menu.classList.add("open");
+    document.body.classList.add("active");
+    navContainer.style.left = "0";
+    navContainer.style.width = "30rem"
+});
+
+navClose.addEventListener("click", () => {
+    menu.classList.remove("open");
+    document.body.classList.remove("active");
+    navContainer.style.left = "-30rem";
+    navContainer.style.width = "0"
+});
+
+
+
+//  AOS
+AOS.init();
